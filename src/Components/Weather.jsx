5bfd1c230b5beed4eb1060706f6a550cc088a0f1
subham8757 <subhammazumdar8757@gmail.com>
2024-08-21 23:34:12 +0530

    update

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -7,7 +7,7 @@ const Weather = () => {
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState([]);
   const [error, setError] = useState("");
-  const [unit, setUnit] = useState("metric"); 
+  const [unit, setUnit] = useState("metric");
 
   const fetchWeatherData = async () => {
     if (!city) {
@@ -22,6 +22,8 @@ const Weather = () => {
 
       if (locationResponse.data.length === 0) {
         setError("City not found.");
+        setForecast([]);
+        setWeather(null);
         return;
       }
 
@@ -52,10 +54,8 @@ const Weather = () => {
     }
   };
 
- 
-
   const handleClick = () => {
-    fetchWeatherData()
+    fetchWeatherData();
   };
 
   const handleUnitToggle = () => {
@@ -63,18 +63,22 @@ const Weather = () => {
   };
 
   const convertTemperature = (temp) => {
-    return unit === "metric"
-      ? temp
-      : temp * 9/5 + 32; 
+    return unit === "metric" ? temp : (temp * 9) / 5 + 32;
   };
+
+  const handleKeyDownSearch = (event) => {
+    if (event.code === "Enter") {
+      handleClick();
+    }
+  };
+
+  
 
   return (
     <div
       className="w-full min-h-screen flex flex-col items-center bg-cover bg-center"
       style={{ backgroundImage: `url('/backgroud.jpg')` }}
     >
-
-
       <h1 className="text-5xl mt-7 text-white font-sans">WEATHER APP</h1>
       <div className="mt-8 w-full max-w-md px-4 py-8 bg-white bg-opacity-80 rounded-lg shadow-lg">
         <input
@@ -84,6 +88,7 @@ const Weather = () => {
           className="border p-3 w-full rounded-md"
           value={city}
           onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(event) => handleKeyDownSearch(event)}
         />
         <button
           className="bg-slate-600 text-white p-3 mt-2 w-full hover:bg-slate-700 rounded-md"
@@ -91,46 +96,73 @@ const Weather = () => {
         >
           Show Weather
         </button>
+      </div>
+      
+      { (weather || error) && <div className="mt-8 w-full max-w-md px-4 py-1 bg-white bg-opacity-80 rounded-lg shadow-lg">
+        {weather && (
+          <label className="inline-flex w-full items-center cursor-pointer mt-4   justify-end">
+            <span className="me-3 text-sm font-medium text-gray-900">
+              {unit === "metric" ? "Celsius" : "Fahrenheit"}
+            </span>
+            <input
+              type="checkbox"
+              className="sr-only peer"
+              checked={unit === "imperial"}
+              onChange={handleUnitToggle}
+            />
+            <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+        )}
 
-        <label className="inline-flex items-center cursor-pointer mt-4">
-          <input
-            type="checkbox"
-            className="sr-only peer"
-            checked={unit === "imperial"}
-            onChange={handleUnitToggle}
-          />
-          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-          <span className="ms-3 text-sm font-medium text-gray-900">
-            {unit === "metric" ? "Celsius" : "Fahrenheit"}
-          </span>
-        </label>
-
-        {error && <p className="text-red-500 mt-2">{error}</p>}
+        {error && (
+          <p className="text-red-500 mt-4 mb-4 w-full text-center">{error}</p>
+        )}
 
         {weather && (
           <div className="mt-4 bg-white p-6 rounded-md shadow-md">
-            <h2 className="text-2xl mb-2 font-semibold">{weather.name}</h2>
+            <div className="flex items-center">
+              <h2 className="text-2xl mb-2 font-semibold">{weather.name}</h2>
+              <div className="ml-2">
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                  alt={weather.weather[0].description}
+                  className="w-12 h-12"
+                />
+              </div>
+            </div>
+
             <div className="flex flex-col items-center md:flex-row md:items-start mt-5">
-              <img
-                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                alt={weather.weather[0].description}
-                className="w-12 h-12"
-              />
-              <div className="ml-0 md:ml-4">
-                <p className="text-lg">
-                  <strong>Temperature:</strong> {convertTemperature(weather.main.temp).toFixed(1)} °{unit === "metric" ? "C" : "F"}
+              <div className="ml-0  w-full">
+                <p className="text-6xl text-center">
+                  {convertTemperature(weather.main.temp).toFixed(1)} °
+                  {unit === "metric" ? "C" : "F"}
                 </p>
-                <p>
-                  <strong>Min Temp:</strong> {convertTemperature(weather.main.temp_min).toFixed(1)} °{unit === "metric" ? "C" : "F"}
-                </p>
-                <p>
-                  <strong>Max Temp:</strong> {convertTemperature(weather.main.temp_max).toFixed(1)} °{unit === "metric" ? "C" : "F"}
-                </p>
+                <div className="flex justify-center gap-12 mt-2 mb-4">
+                  <div>
+                    <p>
+                      <strong>Min:</strong>{" "}
+                      {convertTemperature(weather.main.temp_min).toFixed(1)} °
+                      {unit === "metric" ? "C" : "F"}
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      <strong>Max:</strong>{" "}
+                      {convertTemperature(weather.main.temp_max).toFixed(1)} °
+                      {unit === "metric" ? "C" : "F"}
+                    </p>
+                  </div>
+                </div>
+
                 <p>
                   <strong>Humidity:</strong> {weather.main.humidity} %
                 </p>
                 <p>
-                  <strong>Wind Speed:</strong> {unit === "metric" ? weather.wind.speed : (weather.wind.speed * 2.237).toFixed(1)} {unit === "metric" ? "m/s" : "mph"}
+                  <strong>Wind Speed:</strong>{" "}
+                  {unit === "metric"
+                    ? weather.wind.speed
+                    : (weather.wind.speed * 2.237).toFixed(1)}{" "}
+                  {unit === "metric" ? "m/s" : "mph"}
                 </p>
                 <p>
                   <strong>Wind Direction:</strong> {weather.wind.deg}°
@@ -146,7 +178,7 @@ const Weather = () => {
         {forecast.length > 0 && (
           <div className="mt-8 bg-white p-6 rounded-md shadow-md">
             <h2 className="text-2xl mb-2 font-semibold">5-Day Forecast</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
               {forecast.map((day, index) => (
                 <div key={index} className="border p-5 rounded-md shadow-sm">
                   <h3 className="text-lg mb-2">{day.date}</h3>
@@ -156,7 +188,9 @@ const Weather = () => {
                     className="w-10 h-10"
                   />
                   <p>
-                    <strong>Average Temp:</strong> {convertTemperature(day.temp).toFixed(1)} °{unit === "metric" ? "C" : "F"}
+                    <strong>Average Temp:</strong>{" "}
+                    {convertTemperature(day.temp).toFixed(1)} °
+                    {unit === "metric" ? "C" : "F"}
                   </p>
                   <p>
                     <strong>Description:</strong> {day.description}
@@ -166,7 +200,7 @@ const Weather = () => {
             </div>
           </div>
         )}
-      </div>
+      </div>}
     </div>
   );
 };
